2a97bf3596b68c845fdc9fee0b538a67b4589ee7
mac-poco256 <pocodev@harukago.sakura.ne.jp>
2025-07-12 06:20:39 +0900

    fix: fix an issue.
    fix an issue that `setAcceptsTouchEvents` is deprecated.

--- src/mainview/PoCoView.m
+++ src/mainview/PoCoView.m
@@ -378,7 +378,7 @@ static const int ZOOM_FACTOR[] = {      // 表示倍率テーブル(0.1%単位)
 
 #if (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6)
         // touch event を受け取る
-        [self setAcceptsTouchEvents:YES];
+        [self setAllowedTouchTypes:(NSTouchTypeMaskDirect | NSTouchTypeMaskIndirect)];
         [self setWantsRestingTouches:YES];
 #endif  // MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6
     }
